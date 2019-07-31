0d8d84bcf0322638b85159431ee111d1e0b376a0
Yanislav <admin@Yanislavs-MacBook-Pro.local>
2019-07-31 10:25:39 +0300

    code refactoring

--- api/app/utils/numberGenerator.js
+++ api/app/utils/numberGenerator.js
@@ -1,7 +1,9 @@
 const MAX = 100;
+
 function generate(count) {
   const resultSet = new Set();
   let i = 0;
+
   while (i < count) {
     const number = Math.floor(Math.random() * Math.floor(MAX))
     if (!resultSet.has(number)) {
@@ -9,6 +11,7 @@ function generate(count) {
       i++;
     }
   }
+  
   return Array.from(resultSet);
 }
 
